54406d9120a818203140865a81184474f928b39e
SeungKu_Kang <kangsg26@naver.com>
2023-11-12 23:51:17 +0900

    * for문

---
+++ 11_for.py
@@ -0,0 +1,19 @@
+days = ["월", "화", "수", "목", "금"]
+
+for day in days:
+    print(f"{day}요일 입니다.")
+
+print("주말이 되었습니다.")
+
+# 문자열을 이용한 경우
+daysStr = "월화수목금"
+
+for day in daysStr:
+    print(f"{day}요일 입니다.")
+
+# range를 이용할 경우
+for i in range(10):
+    print(i)
+
+for i in range(1, 10):
+    print(i)
